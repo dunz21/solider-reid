cf169fe0021117a347716f28620c8da1b23b4abf
Diego Sepulveda <diego.sepulveda@drakefsi.com>
2023-10-11 16:23:32 -0300

    better plots

--- plot_solider.py
+++ plot_solider.py
@@ -1,6 +1,20 @@
-from utils.colab import plot_pca
+from utils.colab import solider_result,plot_pca,plot_tsne,plot_mds,plot_nmf,plot_svd
 
 if __name__ == "__main__":
     folder_path = './images_subframev2/1'
-    test = ['./images_subframev2/1','./images_subframev2/2','./images_subframev2/3']
-    plot_pca(folder_path=test,simpleLegend=True,title='TEST XX',weight='./model/swin_base_market.pth')
+    test = [
+        # './images_subframev2/90',
+        # './images_subframev2/10',
+        # './images_subframev2/44',
+        # './images_subframev2/72',
+        # './images_subframev2/78',
+        './images_subframev2/1',
+        # './images_subframev2/2',
+        './images_testsss',
+        ]
+    features , images_names = solider_result(folder_path=test, weight='./model/swin_base_market.pth')
+    plot_pca(features_array=features, image_names=images_names,simpleLegend=True, title='TEST swin_base_market')
+    # plot_tsne(features_array=features, image_names=images_names,simpleLegend=True, title='TEST swin_base_market')
+    # plot_mds(features_array=features, image_names=images_names,simpleLegend=True, title='TEST swin_base_market')
+    # plot_svd(features_array=features, image_names=images_names,simpleLegend=True, title='TEST swin_base_market')
+    
--- utils/colab.py
+++ utils/colab.py
@@ -8,7 +8,13 @@ from sklearn.decomposition import PCA
 import os
 import glob
 import seaborn as sns
+import numpy as np
 from sklearn.cluster import DBSCAN
+from sklearn.manifold import TSNE
+from sklearn.manifold import MDS
+from sklearn.decomposition import NMF
+from sklearn.decomposition import TruncatedSVD  # Importing TruncatedSVD
+
 
 def solider_model(path):
     cfg.merge_from_file("./configs/market/swin_base.yml")
@@ -46,8 +52,7 @@ def extract_images_from_subfolders(folder_paths):
             all_images.extend(images)
     return all_images
 
-
-def plot_pca(folder_path="",simpleLegend=True, title="",weight=''):
+def solider_result(folder_path="", weight=''):
     model = solider_model(weight)
     images = extract_images_from_subfolders(folder_path)
     # Extract image names from paths
@@ -60,7 +65,9 @@ def plot_pca(folder_path="",simpleLegend=True, title="",weight=''):
     
     # Convert tensor to numpy array
     features_array = features_list.cpu().numpy()
-    
+    return features_array, image_names
+
+def plot_pca(features_array="", image_names=[],simpleLegend=True, title="", figsize=(12,10)):
     # Apply PCA
     pca = PCA(n_components=2)
     pca_result = pca.fit_transform(features_array)
@@ -85,7 +92,7 @@ def plot_pca(folder_path="",simpleLegend=True, title="",weight=''):
     legend_handles_labels = []
 
     # Plotting
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=figsize)
     palette = sns.color_palette("husl", len(unique_prefixes))
     for i, (x, y) in enumerate(pca_result):
         color = palette[prefix_to_index[prefixes[i]]]
@@ -115,5 +122,185 @@ def plot_pca(folder_path="",simpleLegend=True, title="",weight=''):
     plt.xlabel('Principal Component 1')
     plt.ylabel('Principal Component 2')
     plt.title(f"PCA Solider {title}")
+    plt.legend(handles=sorted_handles, labels=sorted_labels)
+    plt.show()
+
+def plot_tsne(features_array="", image_names=[],simpleLegend=True, title="", figsize=(12,10)):
+    # Apply t-SNE
+    tsne = TSNE(n_components=2, perplexity=5, n_iter=300)
+    tsne_result = tsne.fit_transform(features_array)
+    
+    prefixes = [int(name.split('_')[1]) for name in image_names]
+    suffixes = [int(name.split('_')[2]) for name in image_names]
+    max_suffix = max(suffixes)
+    min_alpha = 0.6
+    normalized_suffixes = [min_alpha + (1 - min_alpha) * (s / max_suffix) for s in suffixes]
+    
+    unique_prefixes = list(set(prefixes))
+    prefix_to_index = {prefix: i for i, prefix in enumerate(unique_prefixes)}
+    added_to_legend = set()
+    legend_handles_labels = []
+
+    plt.figure(figsize=figsize)
+    palette = sns.color_palette("husl", len(unique_prefixes))
+    for i, (x, y) in enumerate(tsne_result):
+        color = palette[prefix_to_index[prefixes[i]]]
+        label = None
+        if simpleLegend:
+            if prefixes[i] not in added_to_legend:
+                label = f"img_{prefixes[i]}"
+                added_to_legend.add(prefixes[i])
+        else:
+            label = f"img_{prefixes[i]}_{suffixes[i]}"
+            added_to_legend.add(prefixes[i])
+        
+        plt.text(x, y, f"{prefixes[i]}_{suffixes[i]}", fontsize=8, ha='right', va='bottom')
+        handle = plt.scatter(x, y, color=(color[0], color[1], color[2], normalized_suffixes[i]), label=label)
+        
+        if label:
+            legend_handles_labels.append((handle, label))
+
+    legend_handles_labels = sorted(legend_handles_labels, key=lambda x: x[1])
+    sorted_handles, sorted_labels = zip(*legend_handles_labels)
+
+    plt.xlabel('t-SNE Dimension 1')
+    plt.ylabel('t-SNE Dimension 2')
+    plt.title(f"t-SNE Solider {title}")
+    plt.legend(handles=sorted_handles, labels=sorted_labels)
+    plt.show()
+
+def plot_mds(features_array="", image_names=[],simpleLegend=True, title="", figsize=(12,10)):
+    # Apply MDS
+    mds = MDS(n_components=2)
+    mds_result = mds.fit_transform(features_array)
+    
+    # Extract prefix and suffix from image names for coloring
+    prefixes = [int(name.split('_')[1]) for name in image_names]
+    suffixes = [int(name.split('_')[2]) for name in image_names]
+    max_suffix = max(suffixes)
+    min_alpha = 0.6
+    normalized_suffixes = [min_alpha + (1 - min_alpha) * (s / max_suffix) for s in suffixes]
+    
+    # Create a mapping of prefix to palette index
+    unique_prefixes = list(set(prefixes))
+    prefix_to_index = {prefix: i for i, prefix in enumerate(unique_prefixes)}
+    added_to_legend = set()
+    legend_handles_labels = []
+
+    # Plotting
+    plt.figure(figsize=figsize)
+    palette = sns.color_palette("husl", len(unique_prefixes))
+    for i, (x, y) in enumerate(mds_result):
+        color = palette[prefix_to_index[prefixes[i]]]
+        label = None
+        if simpleLegend:
+            if prefixes[i] not in added_to_legend:
+                label = f"img_{prefixes[i]}"
+                added_to_legend.add(prefixes[i])
+        else:
+            label = f"img_{prefixes[i]}_{suffixes[i]}"
+            added_to_legend.add(prefixes[i])
+        plt.text(x, y, f"{prefixes[i]}_{suffixes[i]}", fontsize=8, ha='right', va='bottom')
+        handle = plt.scatter(x, y, color=(color[0], color[1], color[2], normalized_suffixes[i]), label=label)
+
+        if label:
+            legend_handles_labels.append((handle, label))
+
+    # Sort the handles and labels
+    legend_handles_labels = sorted(legend_handles_labels, key=lambda x: x[1])
+    sorted_handles, sorted_labels = zip(*legend_handles_labels)
+
+    plt.xlabel('MDS Dimension 1')
+    plt.ylabel('MDS Dimension 2')
+    plt.title(f"MDS Solider {title}")
+    plt.legend(handles=sorted_handles, labels=sorted_labels)
+    plt.show()
+
+def plot_nmf(features_array="", image_names=[],simpleLegend=True, title="", figsize=(12,10)):
+    # Apply NMF
+    nmf = NMF(n_components=2)
+    nmf_result = nmf.fit_transform(features_array)
+    
+    prefixes = [int(name.split('_')[1]) for name in image_names]
+    suffixes = [int(name.split('_')[2]) for name in image_names]
+    max_suffix = max(suffixes)
+    min_alpha = 0.6
+    normalized_suffixes = [min_alpha + (1 - min_alpha) * (s / max_suffix) for s in suffixes]
+    
+    unique_prefixes = list(set(prefixes))
+    prefix_to_index = {prefix: i for i, prefix in enumerate(unique_prefixes)}
+    added_to_legend = set()
+    legend_handles_labels = []
+
+    plt.figure(figsize=figsize)
+    palette = sns.color_palette("husl", len(unique_prefixes))
+    for i, (x, y) in enumerate(nmf_result):
+        color = palette[prefix_to_index[prefixes[i]]]
+
+        label = None
+        if simpleLegend:
+            if prefixes[i] not in added_to_legend:
+                label = f"img_{prefixes[i]}"
+                added_to_legend.add(prefixes[i])
+        else:
+            label = f"img_{prefixes[i]}_{suffixes[i]}"
+            added_to_legend.add(prefixes[i])
+        plt.text(x, y, f"{prefixes[i]}_{suffixes[i]}", fontsize=8, ha='right', va='bottom')
+        handle = plt.scatter(x, y, color=(color[0], color[1], color[2], normalized_suffixes[i]), label=label)
+
+        if label:
+            legend_handles_labels.append((handle, label))
+
+    legend_handles_labels = sorted(legend_handles_labels, key=lambda x: x[1])
+    sorted_handles, sorted_labels = zip(*legend_handles_labels)
+
+    plt.xlabel('NMF Component 1')
+    plt.ylabel('NMF Component 2')
+    plt.title(f"NMF Solider {title}")
+    plt.legend(handles=sorted_handles, labels=sorted_labels)
+    plt.show()
+
+def plot_svd(features_array="", image_names=[],simpleLegend=True, title="", figsize=(12,10)):
+    # Apply SVD
+    svd = TruncatedSVD(n_components=2)  # Using TruncatedSVD for dimensionality reduction
+    svd_result = svd.fit_transform(features_array)
+
+    prefixes = [int(name.split('_')[1]) for name in image_names]
+    suffixes = [int(name.split('_')[2]) for name in image_names]
+    max_suffix = max(suffixes)
+    min_alpha = 0.6
+    normalized_suffixes = [min_alpha + (1 - min_alpha) * (s / max_suffix) for s in suffixes]
+    
+    unique_prefixes = list(set(prefixes))
+    prefix_to_index = {prefix: i for i, prefix in enumerate(unique_prefixes)}
+    added_to_legend = set()
+    legend_handles_labels = []
+    
+    # Plotting
+    plt.figure(figsize=figsize)
+    palette = sns.color_palette("husl", len(unique_prefixes))
+    for i, (x, y) in enumerate(svd_result):  # Change pca_result to svd_result
+        color = palette[prefix_to_index[prefixes[i]]]
+
+        label = None
+        if simpleLegend:
+            if prefixes[i] not in added_to_legend:
+                label = f"img_{prefixes[i]}"
+                added_to_legend.add(prefixes[i])
+        else:
+            label = f"img_{prefixes[i]}_{suffixes[i]}"
+            added_to_legend.add(prefixes[i])
+        plt.text(x, y, f"{prefixes[i]}_{suffixes[i]}", fontsize=8, ha='right', va='bottom')
+        handle = plt.scatter(x, y, color=(color[0], color[1], color[2], normalized_suffixes[i]), label=label)
+
+        if label:
+            legend_handles_labels.append((handle, label))
+
+    legend_handles_labels = sorted(legend_handles_labels, key=lambda x: x[1])
+    sorted_handles, sorted_labels = zip(*legend_handles_labels)
+    
+    plt.xlabel('Component 1')
+    plt.ylabel('Component 2')
+    plt.title(f"SVD Solider {title}")  # Change PCA to SVD in the title
     plt.legend(handles=sorted_handles, labels=sorted_labels)
     plt.show()
